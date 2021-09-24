1a6f4a0d4dcdfed4f8262af5742c2f4fef4ee6b9
sraboni303 <sraboni391@gmail.com>
2021-09-24 15:59:20 +0600

    add Nav Component

---
+++ components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark padding">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Firengii</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">My Items</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.padding {
+  padding: 1.5rem;
+  background-color: black;
+}
+.nav-left {
+  margin-left: auto;
+}
+</style>
