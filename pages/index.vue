14ed05d9e8bf2ab7e889eb092f9a5748f05b7a56
sraboni303 <sraboni391@gmail.com>
2021-09-24 14:14:59 +0600

    show LargeCardDisplay component by props

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="container">
     <Hero />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
   </div>
 </template>
+
+<script>
+import { largeCardSections } from "@/static/data.js";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+    };
+  },
+};
+</script>
